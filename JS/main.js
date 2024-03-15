8c1c069480bb69f8f65e83dc4478331456eacc8a
Clark <galaxy.games199@gmail.com>
2024-03-15 20:59:49 -0300

    commit inicial

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
+    // Inicialização do carrossel de imagens usando o plugin "slick"
+    $('#carousel-imagens').slick({
+        autoplay: true, // Auto reprodução ativada
+        arrows: false   // Desativação das setas de navegação
+    });
+
+    // Adiciona um evento de clique ao botão do menu hamburguer para mostrar/esconder a navegação
+    $('.menu-hamburger').click(function () {
+        $('nav').slideToggle(); // Alternância da visibilidade da navegação
+    });
 
     // Aplica uma máscara ao campo de telefone
     $('#telefone').mask('(00) 00000-0000');
-
-    // Aplica uma máscara ao campo de cpf
-    $('#cpf').mask('000.000.000-00');
-
-    // Aplica uma máscara ao campo de cep
-    $('#CEP').mask('00000-000');
 
     // Configura a validação do formulário
     $('form').validate({
@@ -22,18 +26,18 @@ $(document).ready(function () {
             telefone: {
                 required: true // Campo de telefone é obrigatório
             },
-            cpf: {
-                required: true // Campo de CPF é obrigatório
+            mensagem: {
+                required: true // Campo de mensagem é obrigatório
             },
-            endereço: {
-                required: true // Campo de endereço é obrigatório
-            },
-            CEP: {
-                required: true // Campo de CEP é obrigatório
+            veiculoDeInteresse: {
+                required: false // Campo de veículo de interesse não é obrigatório
             }
         },
         messages: {
             nome: 'Por favor, insira o nome' // Mensagem de erro para o campo de nome
+        },
+        submitHandler: function(form) {
+            console.log(form); // Mostra os dados do formulário no console quando enviado com sucesso
         },
         invalidHandler: function(evento, validador) {
             let camposIncorretos = validador.numberOfInvalids();
@@ -42,4 +46,19 @@ $(document).ready(function () {
             }
         }
     });
+});
+
+// Adiciona um evento de clique aos botões dentro da lista de veículos
+$('.lista-veiculos button').click(function(){
+    // Define o destino da rolagem suave e obtém o nome do veículo clicado
+    const destino = $('#contato');
+    const nomeVeiculo = $(this).parent().find('h3').text();
+
+    // Atualiza o valor do campo "veiculo-interesse" com o nome do veículo clicado
+    $('#veiculo-interesse').val(nomeVeiculo);
+
+    // Realiza uma animação de rolagem suave até o elemento de destino
+    $('html').animate({
+        scrollTop: destino.offset().top // Rola até a posição do elemento de destino
+    },1000); // Duração da animação: 1000 milissegundos (1 segundo)
 });
